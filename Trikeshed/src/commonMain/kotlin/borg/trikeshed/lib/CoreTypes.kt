660df2cd5386ee9219e54fa803c136bc23c66b53
Jim Northrup <jnorthrup@users.noreply.github.com>
2025-06-27 18:40:36 -0400

    nz

--- Trikeshed/src/commonMain/kotlin/borg/trikeshed/lib/CoreTypes.kt
+++ Trikeshed/src/commonMain/kotlin/borg/trikeshed/lib/CoreTypes.kt
@@ -624,3 +624,33 @@ fun String.toIdx(): Indexed<Char> = length j { this[it] }
 
 @Deprecated("causes accidents leaking the internals")
 suspend fun <T> Indexed<T>.memoize(): Indexed<T> = apply {}
+// CZero utility functions for zero/non-zero checks
+val Byte.nz: Boolean get() = 0 != this.toInt()
+val Short.nz: Boolean get() = 0 != this.toInt()
+val Char.nz: Boolean get() = 0 != this.code
+val Int.nz: Boolean get() = 0 != this
+val Long.nz: Boolean get() = 0L != this
+val UByte.nz: Boolean get() = 0 != this.toInt()
+val UShort.nz: Boolean get() = 0 != this.toInt()
+val UInt.nz: Boolean get() = 0U != this
+val ULong.nz: Boolean get() = 0UL != this
+
+val Byte.z: Boolean get() = 0 == this.toInt()
+val Short.z: Boolean get() = 0 == this.toInt()
+val Char.z: Boolean get() = 0 == this.code
+val Int.z: Boolean get() = 0 == this
+val Long.z: Boolean get() = 0L == this
+val UByte.z: Boolean get() = 0 == this.toInt()
+val UShort.z: Boolean get() = 0 == this.toInt()
+val UInt.z: Boolean get() = 0U == this
+val ULong.z: Boolean get() = 0UL == this
+
+val Boolean.bool: Int get() = if (this) 1 else 0
+
+// Debug utility function
+infix fun <T> T.d(other: T): T {
+    assert(false){}catch{
+        println(other)
+        return this
+    }
+}
