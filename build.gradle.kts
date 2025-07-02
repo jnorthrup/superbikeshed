d49b5b15d90e6a59f879367d233fdc573c08897c
Jim Northrup <jnorthrup@users.noreply.github.com>
2025-07-02 09:03:44 -0400

    2.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("com.github.ben-manes.versions") version "0.51.0"
-    kotlin("multiplatform") version "2.1.21" apply false
-    kotlin("plugin.serialization") version "2.1.21" apply false
+    id("com.github.ben-manes.versions") version "0.52.0"
+    kotlin("multiplatform") version "2.2.0" apply false
+    kotlin("plugin.serialization") version "2.2.0" apply false
 }
 
 allprojects {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,7 +13,7 @@ pluginManagement {
         google()
     }
     plugins {
-        kotlin("multiplatform") version "2.0.0"
+        kotlin("multiplatform") version "2.1.21"
         id("com.github.ben-manes.versions") version "0.51.0"
         id("io.gitlab.arturbosch.detekt") version "1.23.0"
     }
